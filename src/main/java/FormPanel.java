d9f3ddf70165bc5e7cd1d9370818c1c5124f9489
Benjamin Johnson <bencj96@gmail.com>
2019-01-19 12:31:22 -0500

    Added borders and new buttons!

--- src/main/java/FormPanel.java
+++ src/main/java/FormPanel.java
@@ -3,10 +3,25 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 public class FormPanel extends JPanel {
+
+    private JLabel nameLable;
+    private  JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okButton;
+
+
     public FormPanel(){
         Dimension dim = getPreferredSize();
         dim.width = 250;
         setPreferredSize(dim);
+
+        nameLable = new JLabel("Name: ");
+        occupationLabel = new JLabel("Occupation: ");
+
+        // Columns parameter is the width of the field
+        nameField = new JTextField(10);
+        occupationField = new JTextField(10);
 
         Border innerBorder = BorderFactory.createTitledBorder(("Add Person"));
         Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
