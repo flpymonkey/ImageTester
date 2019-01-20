84f252a0c9c30529555d8f489c03c5f8a3be77b9
Benjamin Johnson <bencj96@gmail.com>
2019-01-20 12:25:21 -0500

    Added Insets

--- src/main/java/FormPanel.java
+++ src/main/java/FormPanel.java
@@ -33,37 +33,48 @@ public class FormPanel extends JPanel {
 
         GridBagConstraints gc = new GridBagConstraints();
 
+        // First Row
+        gc.weightx = 1;
+        gc.weighty = 0.1;
         // Always set these and set them properly because they can cause problems later!
         gc.gridx = 0;
         gc.gridy = 0;
-        gc.weightx = 1;
-        gc.weighty = 1;
         gc.fill = GridBagConstraints.NONE;
-        //gc.anchor = GridBagConstraints.LINE_END;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0, 0,0, 5);
         add(nameLable, gc);
 
         // retype to make clear what is happening
         gc.gridx = 1;
         gc.gridy = 0;
         gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = new Insets(0, 0,0,0);
         add(nameField, gc);
 
+        // Second Row
+        gc.weightx = 1;
+        gc.weighty = 0.1;
         // retype to make clear what is happening
         gc.gridx = 0;
         gc.gridy = 1;
         gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0, 0,0, 5);
         add(occupationLabel, gc);
 
         // retype to make clear what is happening
         gc.gridx = 1;
         gc.gridy = 1;
         gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = new Insets(0, 0,0,0);
         add(occupationField, gc);
 
+        // Third Row
+        gc.weightx = 1;
+        gc.weighty = 2.0;
         // retype to make clear what is happening
         gc.gridx = 1;
         gc.gridy = 2;
-        gc.anchor = GridBagConstraints.LINE_START;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
         add(okButton, gc);
     }
 }
